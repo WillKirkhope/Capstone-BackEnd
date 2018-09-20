d7bb072bd849563aaf3760e7e5b1d93dd369d839
WillKirkhope <kirkhopetraining@yahoo.com>
2018-09-20 13:52:46 -0600

    Added changes to app.js

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
-const express = require('express')
+// const express = require('express')
+// const app = express()
+// const cors = require('cors')
+// const bodyParser = require('body-parser')
+// const port = parseInt(process.env.PORT || 3000)
+//
+// app.use(cors())
+// app.use(bodyParser.json())
+//
+// app.get('/', (req,res,next) => {
+//       res.json({
+//           message: 'Hello World!'
+//       });
+//     })
+//
+// app.listen(port, () => {console.log(`Listening on port ${port}`)})
+
+
+const express = require("express")
 const app = express()
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const port = parseInt(process.env.PORT || 3000)
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const port = process.env.PORT || 9000
+const users = require("./routes/users")
+const posts = require("./routes/posts")
+
 
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/', (req,res,next) => {
-      res.json({
-          message: 'Hello World!'
-      });
-    })
+app.use("/users", users)
+app.use("/posts", posts)
 
-app.listen(port, () => {console.log(`Listening on port ${port}`)})
+
+app.listen(port, () => {
+    console.log(`I am listening on ${port}`)
+})
+
+
+module.exports = app;
